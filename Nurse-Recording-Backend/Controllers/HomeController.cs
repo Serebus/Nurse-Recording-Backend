38632a4bc1b000b2aeccf167d167528e75ea0e39
Serebus <revontheusmercer@gmail.com>
2026-04-09 15:42:26 +0800

    First Commit

---
+++ Nurse-Recording-Backend/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Nurse_Recording_Backend.Models;
+
+namespace Nurse_Recording_Backend.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Nurse-Recording-Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Nurse_Recording_Backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
